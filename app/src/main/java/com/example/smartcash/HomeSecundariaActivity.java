133813a223bc19f9c8e53ecfbf8f47042feb399a
Joao Henrique <404joaohbatista.com.br>
2022-04-24 14:18:44 -0300

    Tela Home Secundaria

--- app/src/main/java/com/example/smartcash/HomeSecundariaActivity.java
+++ app/src/main/java/com/example/smartcash/HomeSecundariaActivity.java
@@ -6,11 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class HomePadraoActivity extends AppCompatActivity {
+public class HomeSecundariaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_home_secundaria);
+    }
+
+
+    public void btnCarteiraProfissional(View view){
+        Intent intent = new Intent(this, HomePadraoActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/smartcash/MainActivity.java
+++ app/src/main/java/com/example/smartcash/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_home_secundaria); //tenho q mudar pro login
     }
 }
