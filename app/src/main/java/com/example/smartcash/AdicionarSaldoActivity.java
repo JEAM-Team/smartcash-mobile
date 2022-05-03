4bbf3ead97d528dd59e3e67bad329411aa537ce1
Joao Henrique <404joaohbatista.com.br>
2022-05-03 11:14:19 -0300

    criacao da tela de adiconarSaldo

---
+++ app/src/main/java/com/example/smartcash/AdicionarSaldoActivity.java
@@ -0,0 +1,14 @@
+package com.example.smartcash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdicionarSaldoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicionar_saldo);
+    }
+}
