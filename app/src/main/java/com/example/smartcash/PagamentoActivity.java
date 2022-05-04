046619fee3915356462ddfd1a966e01c178fb5e7
Henrique Maia <henriquemaia8725@hotmail.com>
2022-05-04 11:40:55 -0300

    Criação da tela Saldo e Pagamento

--- app/src/main/java/com/example/smartcash/PagamentoActivity.java
+++ app/src/main/java/com/example/smartcash/PagamentoActivity.java
@@ -2,12 +2,9 @@ package com.example.smartcash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class PagamentoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/smartcash/SaldoActivity.java
+++ app/src/main/java/com/example/smartcash/SaldoActivity.java
@@ -2,16 +2,13 @@ package com.example.smartcash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class SaldoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pagamento);
+        setContentView(R.layout.activity_saldo);
     }
 }
