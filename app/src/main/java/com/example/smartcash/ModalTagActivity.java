ebce93bcbc2e67801779e26477a1b4d90f611b89
Henrique Maia <henriquemaia8725@hotmail.com>
2022-05-14 19:14:55 -0300

    Criação do modal tag

---
+++ app/src/main/java/com/example/smartcash/ModalTagActivity.java
@@ -0,0 +1,14 @@
+package com.example.smartcash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModalTagActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modal_tag);
+    }
+}
