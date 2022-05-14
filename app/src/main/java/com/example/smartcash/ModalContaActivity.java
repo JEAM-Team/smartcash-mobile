6fe51f55dbd1b8ab9c3d7d39d710e5a77f9fb2eb
Henrique Maia <henriquemaia8725@hotmail.com>
2022-05-14 20:36:54 -0300

    Criação da modal conta

---
+++ app/src/main/java/com/example/smartcash/ModalContaActivity.java
@@ -0,0 +1,14 @@
+package com.example.smartcash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModalContaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modal_conta);
+    }
+}
