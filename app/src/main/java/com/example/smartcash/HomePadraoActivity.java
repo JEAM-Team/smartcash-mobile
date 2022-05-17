a2189b5417faafa7cfcca10b13d1649fe11b404d
aiseu <henriquemaia8725@hotmail.com>
2022-05-16 22:22:34 -0300

    Criação do corpo do request e do gson

--- app/src/main/java/com/example/smartcash/HomePadraoActivity.java
+++ app/src/main/java/com/example/smartcash/HomePadraoActivity.java
@@ -1,5 +1,6 @@
 package com.example.smartcash;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,8 +9,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.smartcash.models.views.TotalView;
+import com.google.gson.Gson;
+
+import java.io.IOException;
 
 import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -20,6 +28,7 @@ public class HomePadraoActivity extends AppCompatActivity {
     private final MediaType mediaType = MediaType.parse("application/json");
     Button btnHistoricoPessoal,btnSaldoPessoal,btnPagamentosPessoal;
     TextView textView, textView3, textView9 , textView8;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +41,32 @@ public class HomePadraoActivity extends AppCompatActivity {
         textView3 = findViewById(R.id.textView3);
         textView9 = findViewById(R.id.textView9);
         textView8 = findViewById(R.id.textView8);
+        chamada();
 
     }
 
-    Request request = new Request.Builder()
-            .url("https://smartcash-engine.herokuapp.com/engine/v1/usuario/cadastro")
-            .get()
-            .addHeader("Content-Type", "application/json")
-            .build();
+    public void chamada(){
+        Request request = new Request.Builder()
+                .url("https://smartcash-engine.herokuapp.com/engine/v1/nota/total")
+                .get()
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Authorization","")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure( Call call, IOException e) {
+            }
+
+            @Override
+            public void onResponse( Call call, Response response) throws IOException {
+                  TotalView totalView = new Gson().fromJson(response.body().toString(), TotalView.class );
+
+                  textView.setText(totalView.getTotalRecebimento().toString());
+                  textView3.setText(totalView.getTotalPagamento().toString());
+            }
+        });
+    }
 
     public void btnAbrirHistorico(View view){
         Intent intent = new Intent(this, HistoricoActivity.class);
