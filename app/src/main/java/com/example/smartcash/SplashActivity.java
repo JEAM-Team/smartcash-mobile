63522f7259c5705e6b1cf838f62dfddb59194863
aiseu <henriquemaia8725@hotmail.com>
2022-05-18 21:57:57 -0300

    correção da splash

--- app/src/main/java/com/example/smartcash/SplashActivity.java
+++ app/src/main/java/com/example/smartcash/SplashActivity.java
@@ -39,7 +39,7 @@ public class SplashActivity extends AppCompatActivity {
     }
 
     private void gotoMainActivity() {
-        Intent intent = new  Intent(getApplicationContext(), HomePadraoActivity.class);
+        Intent intent = new  Intent(getApplicationContext(), MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
         finish();
