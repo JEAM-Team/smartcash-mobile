8d9e974589b7b938912a461f43e84decc5f5d977
aiseu <henriquemaia8725@hotmail.com>
2022-05-22 01:46:38 -0300

    Correção

--- app/src/main/java/com/example/smartcash/MainActivity.java
+++ app/src/main/java/com/example/smartcash/MainActivity.java
@@ -62,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void AbrirHome() {
-        Intent intent = new Intent(this, ModalProdutoActivity.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 
